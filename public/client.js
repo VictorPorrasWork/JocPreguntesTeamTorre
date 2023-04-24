a2532130ef80dc255d436f05b106722a2dda928d
Victor <victor.porras.work@gmail.com>
2023-04-24 23:34:55 +0200

    la primera pregunta

--- public/client.js
+++ public/client.js
@@ -3,6 +3,12 @@ const socket = io();
 const joinGameForm = document.getElementById('join-game-form');
 const usernameInput = document.getElementById('username-input');
 
+const preguntaInput = document.getElementById('pregunta');
+const opcionAButton = document.getElementById('opcion-a');
+const opcionBButton = document.getElementById('opcion-b');
+const opcionCButton = document.getElementById('opcion-c');
+const opcionDButton = document.getElementById('opcion-d');
+
 joinGameForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const username = usernameInput.value.trim();
@@ -27,34 +33,69 @@ socket.on('player-list', (players) => {
   });
 });
 
+function cargarPrimeraPregunta() {
+  fetch('/../preguntas.json')
+    .then(response => response.json())
+    .then(preguntas => {
+      const primeraPregunta = preguntas[0];
+      preguntaInput.value = primeraPregunta.pregunta;
+      opcionAButton.textContent = primeraPregunta.opciones.A;
+      opcionBButton.textContent = primeraPregunta.opciones.B;
+      opcionCButton.textContent = primeraPregunta.opciones.C;
+      opcionDButton.textContent = primeraPregunta.opciones.D;
+    })
+    .catch(error => {
+      console.error('Error al cargar la primera pregunta:', error);
+    });
+}
+
+socket.on('connect', () => {
+  cargarPrimeraPregunta();
+});
+
 socket.on('start-game', () => {
   window.location.href = '/juego.html';
 });
 
-socket.on('fase-pregunta', () => {
-
-  // Obtener el div que contendrá la pregunta
-const preguntaDiv = document.getElementById("pregunta");
-
-// Cargar el archivo preguntas.json utilizando fetch
-fetch("/../preguntas.json")
-  .then(response => response.json())
-  .then(data => {
-    // Obtener la primera pregunta
-    const primeraPregunta = data[0];
-    // Mostrar la pregunta en el div correspondiente
-    preguntaDiv.innerText = primeraPregunta.pregunta;
-  });
-
-console.log("pene")
+socket.on('pregunta', (pregunta) => {
+  preguntaInput.value = pregunta.pregunta;
+  opcionAButton.textContent = pregunta.opciones.A;
+  opcionBButton.textContent = pregunta.opciones.B;
+  opcionCButton.textContent = pregunta.opciones.C;
+  opcionDButton.textContent = pregunta.opciones.D;
 });
+
+socket.on('resultado', (resultado) => {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.textContent = resultado;
+});
+
+opcionAButton.addEventListener('click', () => {
+  const respuesta = opcionAButton.textContent;
+  socket.emit('respuesta', respuesta);
+});
+
+opcionBButton.addEventListener('click', () => {
+  const respuesta = opcionBButton.textContent;
+  socket.emit('respuesta', respuesta);
+});
+
+opcionCButton.addEventListener('click', () => {
+  const respuesta = opcionCButton.textContent;
+  socket.emit('respuesta', respuesta);
+});
+
+opcionDButton.addEventListener('click', () => {
+  const respuesta = opcionDButton.textContent;
+  socket.emit('respuesta', respuesta);
+});
+
 
 socket.on('player-disconnected', (username) => {
   const playerList = document.getElementById('player-list');
   const li = playerList.querySelector(':contains("' + username + '")');
   if (li) {
     playerList.removeChild(li);
-}
+  }
+
 });
-
-
